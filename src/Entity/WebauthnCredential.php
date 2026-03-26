9205156f6822f88a91f6a12185a8d24e521bf6d0
Yassine Ben Amor <167880456+yassineBenAmor2002@users.noreply.github.com>
2026-03-26 18:37:47 +0100

    db: ajout entité WebauthnCredential

--- src/Entity/WebauthnCredential.php
+++ src/Entity/WebauthnCredential.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use App\Entity\User; // <- juste l'import, pas de class ici !
 
@@ -25,6 +26,18 @@ class WebauthnCredential
     #[ORM\ManyToOne(targetEntity: User::class)]
     private $user;
 
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $credentialData = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $lastUsedAt = null;
+
     public function getId(): ?int { return $this->id; }
     public function getCredentialId(): string { return $this->credentialId; }
     public function setCredentialId(string $id): self { $this->credentialId = $id; return $this; }
@@ -34,4 +47,52 @@ class WebauthnCredential
     public function setCounter(int $c): self { $this->counter = $c; return $this; }
     public function getUser() { return $this->user; }
     public function setUser($user): self { $this->user = $user; return $this; }
+
+    public function getCredentialData(): ?string
+    {
+        return $this->credentialData;
+    }
+
+    public function setCredentialData(string $credentialData): static
+    {
+        $this->credentialData = $credentialData;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getLastUsedAt(): ?\DateTimeImmutable
+    {
+        return $this->lastUsedAt;
+    }
+
+    public function setLastUsedAt(\DateTimeImmutable $lastUsedAt): static
+    {
+        $this->lastUsedAt = $lastUsedAt;
+
+        return $this;
+    }
 }
